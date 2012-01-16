f56d1b79e63fc30d88dd4f492da7768a7f0fb621
Nathan Hartman <nathanial.hartman@gmail.com>
2012-01-16 12:24:04 -0600

    improved api for example

--- GLGraph.NET.Example/MainWindow.xaml.cs
+++ GLGraph.NET.Example/MainWindow.xaml.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GLGraph.NET.Example {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            if (DesignerProperties.GetIsInDesignMode(this)) return;
+
+            graph.AddLine(new Line(1.0f, Colors.Black, new[] { 
+                new Point(0,0),
+                new Point(1,1),
+                new Point(2,0), 
+                new Point(3,1), 
+                new Point(4,0), 
+            }));
+            graph.Display(new Rect(0, -2, 10, 4));
+            graph.Draw();
         }
     }
 }
--- GLGraph.NET/LineGraph.xaml.cs
+++ GLGraph.NET/LineGraph.xaml.cs
@@ -8,13 +8,52 @@ using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
 namespace GLGraph.NET {
-    public partial class LineGraph : ILineGraph  {
+
+    public class Line {
+        public Point[] Points { get; set; }
+        public Color Color { get; set; }
+        public float Thickness { get; set; }
+        public Line(float thickness, Color color, Point[] points) {
+            Color = color;
+            Points = points;
+            Thickness = thickness;
+        }
+    }
+
+    public class GraphWindow {
+        public int WindowHeight { get; set; }
+        public int WindowWidth { get; set; }
+        public double Top { get; set; }
+        public double Bottom { get; set; }
+        public double Start { get; set; }
+        public double Finish { get; set; }
+    }
+
+    public interface ILineGraph {
+        void Draw();
+
+        void Display(Rect rect);
+
+        void AddMarker(IDrawable marker);
+        void ClearMarkers();
+        IDrawable[] Markers { get; }
+
+        void AddLine(Line newLine);
+        void RemoveLine(Line oldLine);
+
+        void Cleanup();
+
+    }
+
+    public partial class LineGraph : ILineGraph {
         readonly IList<IDrawable> _markers = new List<IDrawable>();
+        readonly IList<Line> _lines = new List<Line>();
+
         bool _panningStarted;
         TickBar _leftTickBar;
         TickBar _bottomTickBar;
         int _xstart, _ystart;
-        IList<Line> _lines;
+
 
         readonly IDictionary<Line, DisplayList> _displayLists = new Dictionary<Line, DisplayList>();
 
@@ -39,37 +78,6 @@ namespace GLGraph.NET {
             _leftTickBar.Dispose();
             _bottomTickBar.Dispose();
             _glcontrol.Dispose();
-        }
-
-        void InitializeUserControl() {
-            _glcontrol = new GLControl();
-            Child = _glcontrol;
-            _glcontrol.Resize += (s, args) => SetWindowSize(_glcontrol.Width, _glcontrol.Height);
-            _glcontrol.MouseDown += (s, args) => StartPan(args.X, args.Y);
-            _glcontrol.MouseMove += (s, args) => Pan(args.X, args.Y);
-            _glcontrol.MouseUp += (s, args) => StopPan();
-            _glcontrol.MouseWheel += (s, args) => Zoom(args.Delta);
-            _glcontrol.Paint += (s, args) => Draw();
-            Loaded += (s, args) => Draw();
-        }
-
-        void InitializeOpenGL() {
-            _glcontrol.MakeCurrent();
-
-            GL.Enable(EnableCap.Texture2D);
-            GL.Enable(EnableCap.LineSmooth);
-            GL.Enable(EnableCap.Blend);
-            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
-            GL.Hint(HintTarget.LineSmoothHint, HintMode.DontCare);
-
-            _leftTickBar = new TickBar(TickBarOrientation.Vertical) {
-                MinorTick = 1,
-                MajorTick = 5
-            };
-            _bottomTickBar = new TickBar(TickBarOrientation.Horizontal) {
-                MinorTick = 1,
-                MajorTick = 5
-            };
         }
 
         public void Draw() {
@@ -139,7 +147,75 @@ namespace GLGraph.NET {
             Draw();
         }
 
-        public void SetWindowSize(int windowWidth, int windowHeight) {
+        public void Display(Rect rect) {
+            if (Window == null) {
+                Window = new GraphWindow();
+            }
+            Window.Start = rect.X;
+            Window.Finish = rect.X + rect.Width;
+            Window.Bottom = rect.Y;
+            Window.Top = rect.Y + rect.Height;
+            Window.WindowWidth = (int)ActualWidth;
+            Window.WindowHeight = (int)ActualHeight;
+
+            _glcontrol.MakeCurrent();
+
+            LoadDisplayLists();
+            Draw();
+        }
+
+        public void RemoveLine(Line oldLine) {
+            _lines.Remove(oldLine);
+            var dl = _displayLists[oldLine];
+            _displayLists.Remove(oldLine);
+            dl.Dispose();
+        }
+
+        public void AddLine(Line newLine) {
+            _lines.Add(newLine);
+            LoadDisplayList(newLine);
+        }
+
+        public void ClearMarkers() {
+            _markers.Clear();
+        }
+
+        public void AddMarker(IDrawable marker) {
+            _markers.Add(marker);
+        }
+
+        void InitializeUserControl() {
+            _glcontrol = new GLControl();
+            Child = _glcontrol;
+            _glcontrol.Resize += (s, args) => SetWindowSize(_glcontrol.Width, _glcontrol.Height);
+            _glcontrol.MouseDown += (s, args) => StartPan(args.X, args.Y);
+            _glcontrol.MouseMove += (s, args) => Pan(args.X, args.Y);
+            _glcontrol.MouseUp += (s, args) => StopPan();
+            _glcontrol.MouseWheel += (s, args) => Zoom(args.Delta);
+            _glcontrol.Paint += (s, args) => Draw();
+            Loaded += (s, args) => Draw();
+        }
+
+        void InitializeOpenGL() {
+            _glcontrol.MakeCurrent();
+
+            GL.Enable(EnableCap.Texture2D);
+            GL.Enable(EnableCap.LineSmooth);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            GL.Hint(HintTarget.LineSmoothHint, HintMode.DontCare);
+
+            _leftTickBar = new TickBar(TickBarOrientation.Vertical) {
+                MinorTick = 1,
+                MajorTick = 5
+            };
+            _bottomTickBar = new TickBar(TickBarOrientation.Horizontal) {
+                MinorTick = 1,
+                MajorTick = 5
+            };
+        }
+
+        void SetWindowSize(int windowWidth, int windowHeight) {
             if (Window != null) {
                 Window.WindowWidth = windowWidth;
                 Window.WindowHeight = windowHeight;
@@ -161,7 +237,7 @@ namespace GLGraph.NET {
             }
         }
 
-         void LoadDisplayList(Line line) {
+        void LoadDisplayList(Line line) {
             _displayLists[line] = new DisplayList(() => {
                 GL.LineWidth(line.Thickness);
                 GL.Begin(BeginMode.Lines);
@@ -171,8 +247,8 @@ namespace GLGraph.NET {
                 for (var j = 0; j < size - 1; j++) {
                     var p1 = line.Points[j];
                     var p2 = line.Points[j + 1];
-                    GL.Vertex2(p1.X / 1000.0, p1.Y);
-                    GL.Vertex2(p2.X / 1000.0, p2.Y);
+                    GL.Vertex2(p1.X, p1.Y);
+                    GL.Vertex2(p2.X, p2.Y);
                 }
                 GL.End();
                 GL.LineWidth(1.0f);
@@ -217,78 +293,8 @@ namespace GLGraph.NET {
             GL.Ortho(0, Window.WindowWidth, 0, Window.WindowHeight, -1, 1);
         }
 
-        public void ReloadDisplay(IList<Line> lines, Rect rect) {
-            if (Window == null) {
-                Window = new GraphWindow();
-            }
-            Window.Start = rect.X - 15;
-            Window.Finish = rect.X + rect.Width - 15;
-            Window.Bottom = rect.Y;
-            Window.Top = rect.Y + rect.Height;
-            Window.WindowWidth = (int)ActualWidth;
-            Window.WindowHeight = (int)ActualHeight;
 
-            _glcontrol.MakeCurrent();
-
-            _lines = new List<Line>(lines);
-            LoadDisplayLists();
-            Draw();
-        }
-
-        public void RemoveLine(Line oldLine){
-            _lines.Remove(oldLine);
-            var dl = _displayLists[oldLine];
-            _displayLists.Remove(oldLine);
-            dl.Dispose();
-        }
-        
-        public void AddLine(Line newLine){
-            _lines.Add(newLine);
-            LoadDisplayList(newLine);
-        }
-
-        public void ClearMarkers() {
-            _markers.Clear();
-        }
-
-        public void AddMarker(IDrawable marker) {
-            _markers.Add(marker);
-        }
     }
 
-    public class Line {
-        public Point[] Points { get; set; }
-        public Color Color { get; set; }
-        public float Thickness { get; set; }
-        public Line(float thickness, Color color, Point[] points) {
-            Color = color;
-            Points = points;
-            Thickness = thickness;
-        }
-    }
 
-    public class GraphWindow {
-        public int WindowHeight { get; set; }
-        public int WindowWidth { get; set; }
-        public double Top { get; set; }
-        public double Bottom { get; set; }
-        public double Start { get; set; }
-        public double Finish { get; set; }
-    }
-
-    public interface ILineGraph {
-        void Draw();
-        void StartPan(int xpos, int ypos);
-        void StopPan();
-        void Pan(int xpos, int ypos);
-        void Zoom(double zdelta);
-        void SetWindowSize(int windowWidth, int windowHeight);
-        void ReloadDisplay(IList<Line> lines, Rect rect);
-        void ClearMarkers();
-        void AddMarker(IDrawable marker);
-        IDrawable[] Markers { get; }
-        void Cleanup();
-        void RemoveLine(Line oldLine);
-        void AddLine(Line newLine);
-    }
 }
